0a1b0ec98d46513b463d9c20bae977bcef69370e
HariomSingh <138313460+Hari0mSingh@users.noreply.github.com>
2023-09-22 17:40:26 +0000

    Datatypes->Number,string,boolean,Null,Undefined,symblo, objects

---
+++ basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version JS
+
+// alert(4+6)// We are using Node JS , Not browser
+
+console.log(9
+    +8) //code redability should be high
+
+let name = "Hariom"//String
+let age = 20//Number
+let isleggedIn = false//Bool
+
+//Premitive Datatypes
+//Number -> 2 to the power 53
+//Bigint also there
+//String -> single & double quotes
+//Boolean -> True & false
+//Null -> Standalone Value 
+//Undefined -> Value Not defined yet
+//symbol -> for uniqueness
+
+//objects
+
+console.log(typeof name)//type->String
+console.log(typeof null)//type-> Null
+console.log(typeof undefined)//type-> undefined
+
