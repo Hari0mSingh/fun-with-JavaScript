19b431fe6e15b0dc804ee7bba62722fd220daa9e
Hariom Singh <hariomsingh0398@gmail.com>
2023-09-27 21:27:22 +0530

    Operations & Comparisions in JS

---
+++ basics/Operations.js
@@ -0,0 +1,35 @@
+//************Operations ***************//
+
+let marks = 6;
+let negMarks = -marks;//Will give negative value
+// console.log(negMarks);
+
+
+let fName = "Hariom";
+let sName = " Singh";
+let fullName = fName + sName;//Concatination of Strings
+// console.log(fullName);
+
+
+//important
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true)
+
+// console.log(true+)//will given error
+
+// console.log(+"")
+
+
+let num1,num2,num3;
+num1=num2=num3=2+4; //Do not use that type of coding
+
+//Your code must be simple and readable
+
+let gameCounter = 100;
+++gameCounter
+gameCounter++
+console.log(gameCounter)
---
+++ basics/conparision.js
@@ -0,0 +1,26 @@
+//Basic comparision
+console.log(2>4);
+console.log(2>=4);
+console.log(2<4);
+console.log(2<=4);
+console.log(2==4);
+console.log(2!=4);
+
+
+console.log("2">1);//true
+console.log("02">1);//true
+console.log("2"==5)//False
+//Try to compare same datatype Elements
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+console.log(null<0);//false
+//Because ==, >= operators works differently in JS
+
+
+//strick check(===) check valuse as well as datatype
+console.log("2"===2);//false
+
+
+//Try to avoid confusion type comparisions
