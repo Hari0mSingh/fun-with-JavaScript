512a1a9c81a952dfe5caadb504bb66554f555c11
Hariom Singh <hariomsingh0398@gmail.com>
2023-10-03 21:11:39 +0530

    string in JS

---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+const Name =  "hariom";
+const repoCount = 10;
+
+// console.log(Name+repoCount+ "Singh");
+
+console.log(`Hello my Name is ${Name} and my repoCount is ${repoCount}`);
+
+const gameName = new String("Hariom Singh");
+
+console.log(gameName[3]);//will print i
+
+console.log(gameName.__proto__);
+
