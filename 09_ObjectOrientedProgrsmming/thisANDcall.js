3805633d7ecfcdd52c0820e79d15f7e048142862
Hariom Singh <hariomsingh0398@gmail.com>
2023-11-04 08:09:36 +0530

    this in js

---
+++ 09_ObjectOrientedProgrsmming/thisANDcall.js
@@ -0,0 +1,13 @@
+//call and this
+function setUsername(username){
+    this.username = username;
+    console.log('called')
+}
+function createUser(username,email,password){
+    setUsername.call(this,username);
+    this.email = email;
+    this.password  = password;
+}
+
+const chai =new createUser('Hariom','xyz@gmail.com','123')
+console.log(chai) 
